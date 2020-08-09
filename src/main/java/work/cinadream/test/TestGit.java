225d58b6a5b9074f7cebbfd0d98a143278ca5cfe
james <thinking_in_java@qq.com>
2020-08-09 12:15:24 +0800

    发生冲突

--- src/main/java/work/cinadream/test/TestGit.java
+++ src/main/java/work/cinadream/test/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("创建分支");
+        System.out.println("发生冲突");
     }
 }
