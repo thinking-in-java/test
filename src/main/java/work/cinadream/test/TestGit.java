4c6e384c6c13bc67a1b5145314b378c1248d3a95
james <thinking_in_java@qq.com>
2020-08-09 12:11:44 +0800

    创建分支

--- src/main/java/work/cinadream/test/TestGit.java
+++ src/main/java/work/cinadream/test/TestGit.java
@@ -3,5 +3,6 @@ package work.cinadream.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("创建分支");
     }
 }
