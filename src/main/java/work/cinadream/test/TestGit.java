c4bb5bf326a9afdc6925e568dce54bf7db1a704b
james <thinking_in_java@qq.com>
2020-08-09 12:16:00 +0800

    发生冲突的位置

--- src/main/java/work/cinadream/test/TestGit.java
+++ src/main/java/work/cinadream/test/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("创建分支");
+        System.out.println("冲突位置");
     }
 }
